90fbdd63ef68e8760aa9b952eade5b1d948fa13d
gs.vancorstanje@student.avans.nl <gs.vancorstanje@student.avans.nl>
2015-01-10 14:53:33 +0100

    Movie aanpassingen

---
+++ Prog5Assessment/Controllers/MovieController.cs
@@ -0,0 +1,125 @@
+﻿using Prog5Assessment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prog5Assessment.Controllers
+{
+    public class MovieController : Controller
+    {
+        private DatabaseSetup context;
+
+        public MovieController()
+        {
+            context = new DatabaseSetup();
+        }
+
+        // Overview page: displays list of all rooms
+        public ActionResult Index()
+        {
+
+            return View(context.Movie.ToList());
+        }
+
+        public ActionResult Delete(int id = -1)
+        {
+            var room = context.Room.SingleOrDefault(x => x.Id == id);
+
+            if (room == null)
+            {
+                return HttpNotFound();
+            }
+
+            //verwijderen van referenties
+            var movies = context.Movie.Where(c => c.Id == id).ToList();
+            foreach (var movie in movies)
+            {
+                context.Movie.Remove(movie);
+            }
+
+            // delete room
+            context.Room.Remove(room);
+            context.SaveChanges();
+            Response.Redirect("~/Room/");
+            return null;
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id = -1)
+        {
+            var room = context.Room.SingleOrDefault(x => x.Id == id);
+
+            if (room == null)
+            {
+                return HttpNotFound();
+            }
+            return View(room);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Room room, int id = -1)
+        {
+            var dbRoom = context.Room.SingleOrDefault(x => x.Id == id);
+            if (dbRoom == null)
+            {
+                return HttpNotFound();
+            }
+
+            // limit persons
+            //if (room.MaxPersons != 2 && room.MaxPersons != 3 && room.MaxPersons != 5)
+            //{
+            //    // error
+            //    return View();
+            //}
+
+            dbRoom.Seats = room.Seats;
+            dbRoom.Name = room.Name;
+            context.SaveChanges();
+            Response.Redirect("~/Room/");
+            return null;
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewBag.rooms = context.Room.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Models.Movie movie)
+        {
+            // todo: check of zaal beschikbaar is
+            string roomId = Request.Form["roomsdropdown"];
+            var dbRoom = context.Room.SingleOrDefault(x => x.Id == Int32.Parse(roomId));
+            if (dbRoom == null)
+            {
+                ModelState.AddModelError("Room", "Room does not exist!");
+            }
+            else
+            {
+                var movieList = context.Movie.ToList();
+                //if (movieList.Count() > 0)
+                if(false)
+                {
+                    ModelState.AddModelError("Room", "Room already taken!");
+                }
+                else
+                {
+                    // no errors
+                    context.Movie.Add(movie);
+                    context.SaveChanges();
+                    Response.Redirect("~/Movie/");
+                    return null;
+                }
+            }
+
+            ViewBag.rooms = context.Room.ToList();
+            return View();
+
+        }
+
+    }
+}
--- Prog5Assessment/Models/Movie.cs
+++ Prog5Assessment/Models/Movie.cs
@@ -9,13 +9,13 @@ namespace Prog5Assessment.Models
 {
     public class Movie
     {
-
         [Key]
         public int Id { get; set; }
-        public Room BookedRoom { get; set; }
+        public Room Room { get; set; }
+        [Required]
         public string Name { get; set; }
-        public DateTime Date { get; set; }
         public int Price { get; set; }
+        public DateTime Date { get; set; }
         public int Duration { get; set; }
     }
 }
